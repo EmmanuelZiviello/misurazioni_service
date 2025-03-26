0d9c22282bf7bed42e40ac450f74b944b08649c5
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-26 18:55:32 +0100

    Delete F_taste_misurazioni directory

--- F_taste_misurazioni/namespaces.py
+++
@@ -1,4 +0,0 @@
-from flask_restx import Namespace
-
-paziente_ns = Namespace('paziente', description='paziente operations')
-nutrizionista_ns = Namespace('nutrizionista', description='nutrizionista operations')
